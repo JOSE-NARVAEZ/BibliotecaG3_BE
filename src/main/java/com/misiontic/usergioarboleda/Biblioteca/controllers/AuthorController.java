1d76a8cfcf2c7e8263ecfcb1333c0f461a0b0beb
casierrav-sa <carlos.sierra1.mt@usa.edu.co>
2022-10-03 09:05:05 -0500

    First version of full API REST for Module Author and Editorial

--- src/main/java/com/misiontic/usergioarboleda/Biblioteca/controllers/AuthorController.java
+++ src/main/java/com/misiontic/usergioarboleda/Biblioteca/controllers/AuthorController.java
@@ -1,4 +1,41 @@
 package com.misiontic.usergioarboleda.Biblioteca.controllers;
 
+import com.misiontic.usergioarboleda.Biblioteca.models.Author;
+import com.misiontic.usergioarboleda.Biblioteca.services.AuthorService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/author")
 public class AuthorController {
+    @Autowired
+    private AuthorService authorService;
+
+    @GetMapping("/all")
+    public List<Author> getAllAuthors(){
+        return authorService.getAllAuthors();
+    }
+
+    @GetMapping("/{code}")
+    public Optional<Author> getAuthor(@PathVariable("code") int code){
+        return authorService.getAuthor(code);
+    }
+
+    @PostMapping("/insert")
+    public Author insertAuthor(@RequestBody Author author){
+        return authorService.insertAuthor(author);
+    }
+
+    @PutMapping("/update")
+    public Author updateAuthor(@RequestBody Author author){
+        return authorService.updateAuthor(author);
+    }
+
+    @DeleteMapping("/delete/{code}")
+    public Boolean deleteAuthor(@PathVariable("code") int code){
+        return authorService.deleteAuthor(code);
+    }
 }
--- src/main/java/com/misiontic/usergioarboleda/Biblioteca/controllers/EditorialController.java
+++ src/main/java/com/misiontic/usergioarboleda/Biblioteca/controllers/EditorialController.java
@@ -1,4 +1,41 @@
 package com.misiontic.usergioarboleda.Biblioteca.controllers;
 
+import com.misiontic.usergioarboleda.Biblioteca.models.Editorial;
+import com.misiontic.usergioarboleda.Biblioteca.services.EditorialService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/editorial")
 public class EditorialController {
+    @Autowired
+    private EditorialService editorialService;
+
+    @GetMapping("/all")
+    public List<Editorial> getAllEditorials(){
+        return editorialService.getAllEditoriales();
+    }
+
+    @GetMapping("/{id}")
+    public Optional<Editorial> getEditorial(@PathVariable("{id}") int id){
+        return editorialService.getEditorial(id);
+    }
+
+    @PostMapping("/insert")
+    public Editorial insertEditorial(@RequestBody Editorial editorial){
+        return editorialService.insertEditorial(editorial);
+    }
+
+    @PutMapping("/update")
+    public Editorial updateEditorial(@RequestBody Editorial editorial){
+        return editorialService.updateEditorial(editorial);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public Boolean deleteEditorial(@PathVariable("id") int id){
+        return editorialService.deleteEditorial(id);
+    }
 }
