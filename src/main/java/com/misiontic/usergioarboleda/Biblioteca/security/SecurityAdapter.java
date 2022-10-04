4a999b55d99ef767069e99f82c16d258e99eb9b7
casierrav-sa <carlos.sierra1.mt@usa.edu.co>
2022-10-04 11:41:46 -0500

    Adjust security adapter to define patterns of access for authenticated users

--- src/main/java/com/misiontic/usergioarboleda/Biblioteca/security/SecurityAdapter.java
+++ src/main/java/com/misiontic/usergioarboleda/Biblioteca/security/SecurityAdapter.java
@@ -9,12 +9,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class SecurityAdapter extends WebSecurityConfigurerAdapter {
     @Override
-    protected void configure(HttpSecurity http){
-        a -> a.antMatches(
-            "/", "/error", "/webjars/**", "/editorial/**", "/author/**", "/book/**"
-        ).permitAll().anyRequest().authenticated())
-        .exceptHandling(
-                e -> e.authenticatedEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests(
+            a -> a.antMatchers(
+                    "/", "/error", "/webjars/**", "/editorial/**", "/author/**", "/book/**"
+            ).permitAll().anyRequest().authenticated()
+        ).exceptionHandling(
+                e -> e.authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
         ).oauth2Login();
         http.cors().and().csrf().disable();
     }
