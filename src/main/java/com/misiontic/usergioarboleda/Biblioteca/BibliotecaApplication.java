de0a50326b16c1e5b266a1aaba1134ba0cbeb8e0
casierrav-sa <carlos.sierra1.mt@usa.edu.co>
2022-10-05 11:21:50 -0500

    Adjust annotations in main application

--- src/main/java/com/misiontic/usergioarboleda/Biblioteca/BibliotecaApplication.java
+++ src/main/java/com/misiontic/usergioarboleda/Biblioteca/BibliotecaApplication.java
@@ -2,9 +2,12 @@ package com.misiontic.usergioarboleda.Biblioteca;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
-@SpringBootApplication(exclude={SecurityAutoConfiguration.class})
+@EnableConfigurationProperties
+@EntityScan(basePackages = {"com.misiontic.usergioarboleda.Biblioteca.models"})
+@SpringBootApplication
 public class BibliotecaApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BibliotecaApplication.class, args);
